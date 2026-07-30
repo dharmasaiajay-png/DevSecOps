62ab6605082ec08f414e3003ac23ed15f80fe61e
dharmasaiajay-png <dharmasaiajay@gmail.com>
2026-07-30 10:45:14 +0530

    Update S3 backend configuration for Terraform

--- Jenkins-TF/backend.tf
+++ Jenkins-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket        = "dharma-terraform-state"
-    key           = "jenkins/terraform.tfstate"
-    region        = "ap-south-1"
-    encrypt       = true
-    use_lockfile  = true
+    bucket         = "dharma-terraform-state"
+    key            = "jenkins/terraform.tfstate"
+    region         = "ap-south-2"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
   }
 }
