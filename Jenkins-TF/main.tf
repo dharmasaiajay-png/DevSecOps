de6718509d62b771cd47cf6995391ffaa3bc2a92
dharmasaiajay-png <dharmasaiajay@gmail.com>
2026-09-14 15:26:23 +0530

    Remove output variables from main.tf
    Removed output variables for Jenkins and SonarQube URLs.

--- Jenkins-TF/main.tf
+++ Jenkins-TF/main.tf
@@ -303,27 +303,3 @@ resource "aws_instance" "jenkins_bastion_server" {
     aws_iam_role_policy_attachment.jenkins_policy_attachment
   ]
 }
-
-# -------------------------------------------------------
-# Outputs
-# -------------------------------------------------------
-
-output "jenkins_instance_id" {
-  description = "Jenkins EC2 instance ID"
-  value       = aws_instance.jenkins_bastion_server.id
-}
-
-output "jenkins_public_ip" {
-  description = "Jenkins public IP"
-  value       = aws_instance.jenkins_bastion_server.public_ip
-}
-
-output "jenkins_url" {
-  description = "Jenkins URL"
-  value       = "http://${aws_instance.jenkins_bastion_server.public_ip}:8080"
-}
-
-output "sonarqube_url" {
-  description = "SonarQube URL"
-  value       = "http://${aws_instance.jenkins_bastion_server.public_ip}:9000"
-}
